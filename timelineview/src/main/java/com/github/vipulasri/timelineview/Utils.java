6bee8de3bcf772b88f067442081fa0158ad6ede0
vipulasri <vipulasri.2007@gmail.com>
2018-11-05 09:29:34 +0530

    UPDATE: restrict utils class usage

--- timelineview/src/main/java/com/github/vipulasri/timelineview/Utils.java
+++ timelineview/src/main/java/com/github/vipulasri/timelineview/Utils.java
@@ -2,17 +2,15 @@ package com.github.vipulasri.timelineview;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.support.annotation.RestrictTo;
 import android.util.TypedValue;
 
-@RestrictTo(RestrictTo.Scope.LIBRARY)
-public class Utils {
+class Utils {
 
     public static int dpToPx(float dp, Context context) {
         return dpToPx(dp, context.getResources());
     }
 
-    public static int dpToPx(float dp, Resources resources) {
+    private static int dpToPx(float dp, Resources resources) {
         float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
         return (int) px;
     }
